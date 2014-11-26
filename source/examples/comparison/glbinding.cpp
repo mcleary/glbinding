14683c998bc336a1c21a97bb617918eff743468e
Dominik Mueller <isaak.dominik@gmail.com>
2014-11-26 15:16:33 +0100

    Add example for logging using callbacks.
    Problem with creating RingBuffer so everyone has access and it is not deleted.

--- source/examples/comparison/glbinding.cpp
+++ source/examples/comparison/glbinding.cpp
@@ -2,11 +2,15 @@
 #include "glbinding.h"
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <thread>
 
 #include <glbinding/gl/gl.h>
 
 #include <glbinding/AbstractFunction.h> 
 #include <glbinding/callbacks.h>
+#include <ctime>
 
 
 using namespace gl;
@@ -44,4 +48,58 @@ void glbinding_error(bool enable)
     }
     else
         glbinding::setCallbackMask(glbinding::CallbackMask::None);
+}
+
+void glbinding_log(bool enable, glbinding::RingBuffer<int, 10> * buffer)
+{
+    if (enable)
+    {
+        // std::thread t3([&]()
+        // {
+        //     // time_t  timev;
+        //     // time(&timev);
+        //     // std::string logname = "test_";
+        //     // logname += timev;
+        //     // std::ofstream logfile;
+        //     // logfile.open (logname, std::ios::out);
+
+        //     // std::string entry;
+        //     while(true)
+        //     {
+        //         // while(!buffer.pull(&entry));
+        //         // logfile << entry;
+        //         // logfile.flush();
+        //     }
+
+        // });
+
+        glbinding::setCallbackMask(glbinding::CallbackMask::After | glbinding::CallbackMask::ParametersAndReturnValue);
+        glbinding::setAfterCallback([&](const glbinding::FunctionCall & call) {
+            std::ostringstream os;
+            os << call.function.name() << "(";
+
+            for (unsigned i = 0; i < call.parameters.size(); ++i)
+            {
+                os << call.parameters[i]->asString();
+                if (i < call.parameters.size() - 1)
+                    os << ", ";
+            }
+
+            os << ")";
+
+            if (call.returnValue)
+            {
+                os << " -> " << call.returnValue->asString();
+            }
+
+            os << std::endl;
+            std::string input = os.str();
+            std::cout << input << std::endl;
+            while(!buffer->push(1)){}
+        });
+    }
+    else
+    {
+        glbinding::setCallbackMask(glbinding::CallbackMask::None);
+    }
 }
--- source/tests/glbinding-test/RingBuffer_test.cpp
+++ source/tests/glbinding-test/RingBuffer_test.cpp
@@ -44,6 +44,36 @@ TEST_F(RingBuffer_test, SimpleTest)
     EXPECT_EQ(8, buffer.size());
 }
 
+TEST_F(RingBuffer_test, StringTest)
+{
+    RingBuffer<std::string, 10> buffer;
+
+    std::string result;
+    EXPECT_EQ(false, buffer.pull(&result));
+
+    for (int i = 1; i <= 10; i++)
+    {
+        EXPECT_EQ(true, buffer.push("Hello world!"));
+    }
+    EXPECT_EQ(10, buffer.size());
+    EXPECT_EQ(false, buffer.push("Not working!"));
+
+    for (int i = 1; i <= 5; i++)
+    {
+        EXPECT_EQ(true, buffer.pull(&result));
+        EXPECT_EQ("Hello world!", result);
+    }
+
+    EXPECT_EQ(5, buffer.size());
+
+    for (int i = 1; i <= 3; i++)
+    {
+        EXPECT_EQ(true, buffer.push("Hello world2!"));
+    }
+
+    EXPECT_EQ(8, buffer.size());
+}
+
 TEST_F(RingBuffer_test, MultiThreadedTest)
 {
 
@@ -74,57 +104,57 @@ TEST_F(RingBuffer_test, MultiThreadedTest)
     EXPECT_EQ(0, buffer.size());
 }
 
-TEST_F(RingBuffer_test, MultiThreadedTest2)
-{
+// TEST_F(RingBuffer_test, MultiThreadedTest2)
+// {
 
-    RingBuffer<int, 7> buffer;
-    std::array<int, 10> i1 = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
-    std::array<int, 10> i2 = {{10, 11, 12, 13, 14, 15, 16, 17, 18, 19}};
-    std::list<int> out;
-    std::thread t1([&]()
-    {
-        for(const int s: i1)
-            while(!buffer.push(s));
-    });
+//     RingBuffer<int, 7> buffer;
+//     std::array<int, 10> i1 = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
+//     std::array<int, 10> i2 = {{10, 11, 12, 13, 14, 15, 16, 17, 18, 19}};
+//     std::list<int> out;
+//     std::thread t1([&]()
+//     {
+//         for(const int s: i1)
+//             while(!buffer.push(s));
+//     });
 
-    std::thread t2([&]()
-    {
-        for(const int s: i2)
-            while(!buffer.push(s));
-    });
+//     std::thread t2([&]()
+//     {
+//         for(const int s: i2)
+//             while(!buffer.push(s));
+//     });
 
-    std::thread t3([&]()
-    {
-        int result;
-        // std::cout << "End: " << i1.size() + i2.size() << std::endl;
-        for(int i = 0; i < i1.size() + i2.size(); i++)
-        {
-            while(!buffer.pull(&result));
-            out.push_front(result);
-            std::cout << "i = " << result << std::endl;
-        }
+//     std::thread t3([&]()
+//     {
+//         int result;
+//         // std::cout << "End: " << i1.size() + i2.size() << std::endl;
+//         for(int i = 0; i < i1.size() + i2.size(); i++)
+//         {
+//             while(!buffer.pull(&result));
+//             out.push_front(result);
+//             std::cout << "i = " << result << std::endl;
+//         }
 
-    });
+//     });
 
-    t1.join();
-    t2.join();
-    t3.join();
+//     t1.join();
+//     t2.join();
+//     t3.join();
 
-    EXPECT_EQ(i1.size() + i2.size(), out.size());
-    EXPECT_EQ(0, buffer.size());
+//     EXPECT_EQ(i1.size() + i2.size(), out.size());
+//     EXPECT_EQ(0, buffer.size());
 
-    out.sort();
-    for(const int s: i1)
-    {
-        EXPECT_EQ(s, out.front());
-        out.pop_front();
-    }
-    for(const int s: i2)
-    {
-        EXPECT_EQ(s, out.front());
-        out.pop_front();
-    }
-}
+//     out.sort();
+//     for(const int s: i1)
+//     {
+//         EXPECT_EQ(s, out.front());
+//         out.pop_front();
+//     }
+//     for(const int s: i2)
+//     {
+//         EXPECT_EQ(s, out.front());
+//         out.pop_front();
+//     }
+// }
 
 
 
