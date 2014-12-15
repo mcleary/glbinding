fa3f531109d45b1aff8aab0dd83a16d47b3038bc
Dominik Mueller <isaak.dominik@gmail.com>
2014-12-15 11:37:31 +0100

    Remove consumer parts from ring buffer
    There was no benefit from passing the function and letting the ring buffer do all the work.

--- source/glbinding/include/glbinding/RingBuffer.hpp
+++ source/glbinding/include/glbinding/RingBuffer.hpp
@@ -16,10 +16,6 @@ class RingBuffer
     const int m_size = n+1;
     std::atomic<unsigned long> m_head;
     std::atomic<unsigned long> m_tail;
-    bool consumerSet;
-    int consumerThreshold;
-    bool consumerWorking;
-    std::function<void (T result)> consume;
 
     unsigned long next(unsigned long current) {
         return (current + 1) % m_size;
@@ -30,33 +26,18 @@ class RingBuffer
     {
         m_head = 0;
         m_tail = 0;
-        consumerSet = false;
-        consumerWorking = false;
     };
     bool push(T);
     bool pull(T*);
     int size();
 
-    void setConsumer(std::function<void (T result)> func, int start = 75);
-    void startConsumer();
-    bool consumerRunning();
-
     bool isFull();
     bool isEmpty();
-
-    protected:
-    bool overThreshold();
 };
 
 template <typename T, unsigned long n> bool RingBuffer<T, n>::push(const T object) {
     unsigned long head = m_head.load(std::memory_order_relaxed);
     unsigned long nextHead = next(head);
-    if(consumerSet && !consumerWorking && overThreshold())
-    {
-      startConsumer();
-      int  = 0;
-
-    }
     if (nextHead == m_tail.load(std::memory_order_acquire)) {
         return false;
     }
@@ -97,47 +78,6 @@ template <typename T, unsigned long n> bool RingBuffer<T, n>::isEmpty() {
         return true;
     }
     return false;
-}
-
-template <typename T, unsigned long n> void RingBuffer<T, n>::setConsumer(std::function<void (T result)> func, int start)
-{
-    consumerSet = true;
-    consumerThreshold = start;
-    consume = func;
-}
-
-template <typename T, unsigned long n> void RingBuffer<T, n>::startConsumer()
-{
-    if(!consumerWorking)
-    {
-        consumerWorking = true;
-        std::thread consumer([&]()
-        {
-            T result;
-            while(pull(&result))
-            {
-                consume(result);
-                std::this_thread::sleep_for(std::chrono::milliseconds(200));
-                // std::cout << "Consumed:" << result << std::endl;
-            }
-
-            consumerWorking = false;
-            // std::cout << "Finished consuming -> " << consumerWorking << std::endl;
-        });
-        // std::cout << "Started consumer" << std::endl;
-        consumer.detach();
-    }
-}
-
-template <typename T, unsigned long n> bool RingBuffer<T, n>::overThreshold()
-{
-    int cap = size() * 100 / m_size;
-    return cap >= consumerThreshold;
-}
-
-template <typename T, unsigned long n> bool RingBuffer<T, n>::consumerRunning()
-{
-    return consumerWorking;
 }
 
 } // namespace glbinding
