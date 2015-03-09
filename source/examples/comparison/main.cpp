1f653ef835f5b80f7369755b1d457168b28a2e03
Dominik Mueller <isaak.dominik@gmail.com>
2015-03-09 08:50:14 +0100

    Add callback logging example

--- source/examples/comparison/main.cpp
+++ source/examples/comparison/main.cpp
@@ -90,18 +90,18 @@ void compare()
 
 
     std::cout << std::endl << "test: again, now with logging ..." << std::endl;
-    glbinding::logging::log(true);
+    glbinding::logging::start("logs/comparison");
     timer.start("      glbinding ");
 
     for (int i = 0; i < ITERATIONS; ++i)
         glbinding_test();
     
     long double glbinding_avg_log = timer.stop();
-    glbinding::logging::log(false);
+    glbinding::logging::stop();
 
 
     std::cout << std::endl << "test: again, now log access by ringbuffer ..." << std::endl;
-    glbinding::logging::log(true);
+    glbinding::logging::start("logs/comparison2");
     timer.start("      glbinding ");
 
 
@@ -140,29 +140,40 @@ void compare()
 
 
     std::cout << std::endl << "test: again, now log access by callback ..." << std::endl;
-    glbinding::logging::log(true);
+    glbinding::logging::start("logs/comparison3");
     timer.start("      glbinding ");
 
     int count = 0;
-    int count2 = 196608;
 
     glbinding::addCallbackMask(glbinding::CallbackMask::After | glbinding::CallbackMask::ParametersAndReturnValue);
     glbinding::setAfterCallback([&count](const glbinding::FunctionCall & call)
     {
       count++;
     });
-    glbinding::setAfterCallback([&count2](const glbinding::FunctionCall & call)
-    {
-      count2--;
-    });
 
     for (int i = 0; i < ITERATIONS; ++i)
         glbinding_test();
     
     std::cout << "counted: " << count << std::endl;
-    std::cout << "counted: " << count2 << std::endl;
     long double glbinding_avg_cb = timer.stop();
     glbinding::logging::log(false);
+
+    std::cout << std::endl << "test: again, now callback logging ..." << std::endl;
+    std::ofstream logfile;
+    logfile.open ("logs/comparison4", std::ios::out);
+    timer.start("      glbinding ");
+
+    glbinding::addCallbackMask(glbinding::CallbackMask::After | glbinding::CallbackMask::ParametersAndReturnValue);
+    glbinding::setAfterCallback([&logfile](const glbinding::FunctionCall & call)
+    {
+      logfile << call.toString();
+    });
+
+    for (int i = 0; i < ITERATIONS; ++i)
+        glbinding_test();
+
+    long double glbinding_avg_cb_log = timer.stop();
+    logfile.close();
 
 
     std::cout << std::endl << "glbinding/glew decrease:                 " << (glbinding_avg / glew_avg - 1.0) * 100.0 << "%" << std::endl;
@@ -170,6 +181,8 @@ void compare()
     std::cout << std::endl << "glbinding decrease with logging:         " << (glbinding_avg / glbinding_avg_log - 1.0) * 100.0 << "%" << std::endl;
     std::cout << std::endl << "glbinding decrease with rb:         " << (glbinding_avg / glbinding_avg_rb - 1.0) * 100.0 << "%" << std::endl;
     std::cout << std::endl << "glbinding decrease with cb:         " << (glbinding_avg / glbinding_avg_cb - 1.0) * 100.0 << "%" << std::endl;
+    std::cout << std::endl << "glbinding decrease with cb logging:         " << (glbinding_avg / glbinding_avg_cb_log - 1.0) * 100.0 << "%" << std::endl;
+
 
     std::cout << std::endl << "finalizing ..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(3));
